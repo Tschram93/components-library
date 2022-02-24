0debb7423dd4846b5491f25b036cd98cc1f12928
TS93 <tschram93@yahoo.com>
2022-02-24 01:35:46 -0500

    created 3 loading spinners with css inside ./components/spinner

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useEffect } from 'react';
+import './Spinner.css';
+
+const Spinner = () => {
+
+	return (
+		<div>
+			<div className='main spinner-3'>
+				<h2>Hello, here is my Spinner Component!</h2>
+				<p className='hide'>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi amet
+					odit, adipisci iure at facere iste, provident totam, magni error
+					necessitatibus deserunt placeat eaque? Tenetur porro suscipit cum
+					quis, molestiae ducimus quam sunt voluptas! Ex ratione distinctio
+					error? Cumque nihil corrupti inventore optio omnis! Ipsam officiis
+					cumque sequi iure molestias?
+				</p>
+			</div>
+		</div>
+	);
+}
+
+export default Spinner;

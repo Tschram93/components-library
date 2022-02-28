d7141ab742607747fc04a368173af49822159958
TS93 <tschram93@yahoo.com>
2022-02-28 14:17:39 -0500

    rotate property set on hover for card flip effect

--- src/components/FlipCard/FlipCard.js
+++ src/components/FlipCard/FlipCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as FaIcons from 'react-icons/fa';
 import './FlipCard.css';
 
 const FlipCard = () => {
@@ -11,9 +12,23 @@ const FlipCard = () => {
 			<div className='flip-card'>
 				<div className='flip-card-inner'>
 					<div className='flip-card-front'>
-                        <img src={require('../../assets/pexels-suzy-hazelwood-1629236-min.jpg')} alt="astrophotography of distant stars" />
-                    </div>
-					<div className='flip-card-back'></div>
+						<img
+							src={require('../../assets/pexels-suzy-hazelwood-1629236-min.jpg')}
+							alt='astrophotography of distant stars'
+						/>
+					</div>
+					<div className='flip-card-back'>
+						<div className='back-header'>
+							<img
+								src={require('../../assets/pexels-stein-egil-liland-3374210-min.jpg')}
+								alt='astrophotography of distant stars'
+							/>
+						</div>
+						<div className='back-footer'>
+							<h2>Northern Lights</h2>
+							<p>Most commonly visible close to Earth's magnetic poles.</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>

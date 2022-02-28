fc9aa1ffcc4a2b89760548edf3d136a61b8f66da
TS93 <tschram93@yahoo.com>
2022-02-28 14:36:08 -0500

    flip card animation finished with some minimal styling

--- src/components/FlipCard/FlipCard.js
+++ src/components/FlipCard/FlipCard.js
@@ -5,32 +5,41 @@ import './FlipCard.css';
 const FlipCard = () => {
 	return (
 		<section>
-			<div className='title'>
-				<h1>Flip Card</h1>
-				<p>Flip card placeholder text.</p>
-			</div>
-			<div className='flip-card'>
-				<div className='flip-card-inner'>
-					<div className='flip-card-front'>
-						<img
-							src={require('../../assets/pexels-suzy-hazelwood-1629236-min.jpg')}
-							alt='astrophotography of distant stars'
-						/>
-					</div>
-					<div className='flip-card-back'>
-						<div className='back-header'>
+				<div className='title'>
+					<h1>Flip Card</h1>
+					<p>Flip card placeholder text.</p>
+				</div>
+				<div className='flip-card'>
+					<div className='flip-card-inner'>
+						<div className='flip-card-front'>
 							<img
-								src={require('../../assets/pexels-stein-egil-liland-3374210-min.jpg')}
+								src={require('../../assets/pexels-suzy-hazelwood-1629236-min.jpg')}
 								alt='astrophotography of distant stars'
 							/>
 						</div>
-						<div className='back-footer'>
-							<h2>Northern Lights</h2>
-							<p>Most commonly visible close to Earth's magnetic poles.</p>
+						<div className='flip-card-back'>
+							<div className='back-header'>
+								<img
+									src={require('../../assets/pexels-stein-egil-liland-3374210-min.jpg')}
+									alt='astrophotography of distant stars'
+								/>
+							</div>
+							<div className='back-footer'>
+								<h2>Northern Lights</h2>
+								<p>Most commonly visible close to Earth's magnetic poles.</p>
+                                <p className='back-footer-content'><FaIcons.FaIntercom /> <br /> Norway</p>
+                                <div>
+                                <FaIcons.FaStar />
+                                <FaIcons.FaStar />
+                                <FaIcons.FaStar />
+                                <FaIcons.FaStar />
+                                <FaIcons.FaStarHalf />
+                                </div>
+                                
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
 		</section>
 	);
 };

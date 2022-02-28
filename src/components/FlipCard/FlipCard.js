735c7e009bbd8d331054761c392999ac133ba72a
TS93 <tschram93@yahoo.com>
2022-02-28 13:54:58 -0500

    minimal structure for card

--- src/components/FlipCard/FlipCard.js
+++ src/components/FlipCard/FlipCard.js
@@ -2,7 +2,22 @@ import React from 'react';
 import './FlipCard.css';
 
 const FlipCard = () => {
-	return <div>FlipCard</div>;
+	return (
+		<section>
+			<div className='title'>
+				<h1>Flip Card</h1>
+				<p>Flip card placeholder text.</p>
+			</div>
+			<div className='flip-card'>
+				<div className='flip-card-inner'>
+					<div className='flip-card-front'>
+                        <img src={require('../../assets/pexels-suzy-hazelwood-1629236-min.jpg')} alt="astrophotography of distant stars" />
+                    </div>
+					<div className='flip-card-back'></div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default FlipCard;

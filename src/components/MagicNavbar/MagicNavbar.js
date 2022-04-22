9ef46a553d2da1abfc9635f504cf078d99213f44
TS93 <tschram93@yahoo.com>
2022-04-21 20:20:34 -0400

    magicnavbar created list with a anchor tags within them

--- src/components/MagicNavbar/MagicNavbar.js
+++ src/components/MagicNavbar/MagicNavbar.js
@@ -3,7 +3,40 @@ import "./MagicNavbar.css";
 
 const MagicNavbar = () => {
     return (
-        <div>MagicNavbar</div>
+        <div className="navigation">
+            <ul>
+                <li className="list">
+                    <a href="#">
+                        <span className="icon"></span>
+                        <span className="text">Home</span>
+                    </a>
+                </li>
+                <li className="list">
+                    <a href="#">
+                        <span className="icon"></span>
+                        <span className="text">Profile</span>
+                    </a>
+                </li>
+                <li className="list">
+                    <a href="#">
+                        <span className="icon"></span>
+                        <span className="text">Messages</span>
+                    </a>
+                </li>
+                <li className="list">
+                    <a href="#">
+                        <span className="icon"></span>
+                        <span className="text">Photos</span>
+                    </a>
+                </li>
+                <li className="list">
+                    <a href="#">
+                        <span className="icon"></span>
+                        <span className="text">Settings</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
     )
 }
 

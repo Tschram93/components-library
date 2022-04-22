a7f604553043a3149adc41a53dcc75f4e0a8b56a
TS93 <tschram93@yahoo.com>
2022-04-21 20:42:45 -0400

    imported and implemented icons with react-icons package

--- src/components/MagicNavbar/MagicNavbar.js
+++ src/components/MagicNavbar/MagicNavbar.js
@@ -1,44 +1,61 @@
 import React from 'react';
-import * as FaIcons from 'react-icons/io';
-import "./MagicNavbar.css";
+import * as BiIcons from 'react-icons/bi';
+import * as FaIcons from 'react-icons/fa';
+import * as IonIcons from 'react-icons/io';
+import * as IonIcons5 from 'react-icons/io5';
+import './MagicNavbar.css';
 
 const MagicNavbar = () => {
-    return (
-        <div className="navigation">
-            <ul>
-                <li className="list">
-                    <a href="#">
-                        <span className="icon"></span>
-                        <span className="text">Home</span>
-                    </a>
-                </li>
-                <li className="list">
-                    <a href="#">
-                        <span className="icon"></span>
-                        <span className="text">Profile</span>
-                    </a>
-                </li>
-                <li className="list">
-                    <a href="#">
-                        <span className="icon"></span>
-                        <span className="text">Messages</span>
-                    </a>
-                </li>
-                <li className="list">
-                    <a href="#">
-                        <span className="icon"></span>
-                        <span className="text">Photos</span>
-                    </a>
-                </li>
-                <li className="list">
-                    <a href="#">
-                        <span className="icon"></span>
-                        <span className="text">Settings</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    )
-}
+	return (
+		<div className='navigation'>
+			<ul>
+				<li className='list'>
+					<a href='#'>
+						<span className='icon'>
+							<IonIcons.IoIosHome />
+						</span>
+						<span className='text'>Home</span>
+					</a>
+				</li>
+				<li className='list'>
+					<a href='#'>
+						<span className='icon'>
+							{' '}
+							<FaIcons.FaUser />{' '}
+						</span>
+						<span className='text'>Profile</span>
+					</a>
+				</li>
+				<li className='list'>
+					<a href='#'>
+						<span className='icon'>
+							{' '}
+							<BiIcons.BiEnvelope />{' '}
+						</span>
+						<span className='text'>Messages</span>
+					</a>
+				</li>
+				<li className='list'>
+					<a href='#'>
+						<span className='icon'>
+							{' '}
+							<IonIcons5.IoCamera />{' '}
+						</span>
+						<span className='text'>Photos</span>
+					</a>
+				</li>
+				<li className='list'>
+					<a href='#'>
+						<span className='icon'>
+							{' '}
+							<IonIcons5.IoSettingsOutline />{' '}
+						</span>
+						<span className='text'>Settings</span>
+					</a>
+				</li>
+			</ul>
+		</div>
+	);
+};
 
 export default MagicNavbar;
